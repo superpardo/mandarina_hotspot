b448aae38ac65af65ed357655a5f6b5f258f6b2c
Luis Pardiñas <superpardo@gmail.com>
2023-04-11 16:43:41 -0600

    update content types

--- web/sites/default/settings.local.php
+++ web/sites/default/settings.local.php
@@ -2,14 +2,14 @@
 
 // Docksal DB connection settings.
 $databases['default']['default'] = array (
-  'database' => 'mandarina',
-  'username' => 'root',
-  'password' => 'D3v.2023.c4mb14D4',
-  'host' => '57.128.86.143',
+  'database' => getenv('MYSQL_DATABASE'),
+  'username' => getenv('MYSQL_USER'),
+  'password' => getenv('MYSQL_PASSWORD'),
+  'host' => getenv('MYSQL_HOST'),
   'port' => '3306',
   'driver' => 'mysql',
 );
 
-$settings['config_sync_directory'] = '/var/www/html/config/default';
+$settings['config_sync_directory'] = '/var/www/config/default';
 
 $settings['trusted_host_patterns'] = [ '.*' ];
